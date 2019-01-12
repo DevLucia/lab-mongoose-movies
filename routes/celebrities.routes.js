b72313e5b52ff7fa4a75b0c6715e1e8f63bdd426
Roberto <roberto.m.aragon@gmail.com>
2019-01-12 16:07:54 +0100

    create and delete iterations

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const celebritiesController = require('../controllers/celebrities.controller');
 
 router.get('/', celebritiesController.list)
-router.get('/create', celebritiesController.create)
+router.get('/new', celebritiesController.create)
 
-router.post('/create', celebritiesController.doCreate)
+router.post('/new', celebritiesController.doCreate)
 
 router.get('/:id/edit', celebritiesController.edit)
 router.post('/:id/edit', celebritiesController.doEdit)

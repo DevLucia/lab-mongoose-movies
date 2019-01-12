3d999329e72e6e8c1a000b2c717ed5887acf8bec
Lucia Cazorla <a.lucia.cazorla@gmail.com>
2019-01-12 16:10:49 +0100

    merde

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 const celebritiesController = require('../controllers/celebrities.controller');
 
-router.get('/', celebritiesController.list)
-router.get('/create', celebritiesController.create)
+router.get('/', celebritiesController.list);
+router.get('/create', celebritiesController.create);
 
 router.post('/create', celebritiesController.doCreate)
 
